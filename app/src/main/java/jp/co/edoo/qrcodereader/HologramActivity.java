e0d2dd1c2f42fa6fee5017109385bb113750fdb8
TokikawaTeppei <tokikawateppei@MOWifi.local>
2016-03-21 12:32:16 +0800

    initial commit

---
+++ app/src/main/java/jp/co/edoo/qrcodereader/HologramActivity.java
@@ -0,0 +1,68 @@
+package jp.co.edoo.qrcodereader;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+
+public class HologramActivity extends Activity{
+    private AlertDialog yesNoDialog;
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final String qrURl = getIntent().getExtras().getString("qr_resutl");
+        AlertDialog.Builder yesNO = new AlertDialog.Builder(HologramActivity.this);
+        yesNO.setMessage(qrURl).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                setContentView(R.layout.activity_hologram);
+                mWebView = (WebView) findViewById(R.id.wvElement);
+                String data ="<body style=\"width:100%;height:100%;padding:0px;margin:0px;\"><img style=\"width:100%;height:100%;padding:0px;margin:0px;\" src=\"pikachu.gif\"/></body>";
+                mWebView.loadDataWithBaseURL("file:///android_asset/",data,"text/html","utf-8",null);
+
+                dialogInterface.dismiss();
+
+            }
+        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+                Intent intent = new Intent(HologramActivity.this, QRReaderActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        yesNoDialog = yesNO.create();
+        yesNoDialog.show();
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        yesNoDialog.dismiss();
+        yesNoDialog.hide();
+    }
+}
---
+++ app/src/main/java/jp/co/edoo/qrcodereader/QRReaderActivity.java
@@ -0,0 +1,76 @@
+package jp.co.edoo.qrcodereader;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class QRReaderActivity extends Activity {
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Intent intent = new Intent(QRReaderActivity.this,MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrreader);
+        new IntentIntegrator(QRReaderActivity.this).initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scannedResutlt = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if(scannedResutlt != null){
+            String re = scannedResutlt.getContents();
+            //check if there is really a result
+            if(re != null){
+                Log.e("String content: ", re);
+                Intent holoGram = new Intent(QRReaderActivity.this,HologramActivity.class);
+                holoGram.putExtra("qr_resutl",re);
+                holoGram.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(holoGram);
+                finish();
+            }
+
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent = new Intent(QRReaderActivity.this,MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        finish();
+    }
+}
